93b11aaef31230e4e1443d5e2222e99795533236
RodrigoGonzalesAlanoca <rgonzalesa@unjbg.edu.pe>
2026-07-17 22:40:42 -0500

    Actualiza ejercicio 1 de estructuras selectivas: agrega validacion de entrada

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -6,7 +6,10 @@ int main() {
     cout << "Ingrese un numero: ";
     cin >> numero;
 
-    if (numero % 2 == 0) {
+    if (numero < 0) {
+        cout << "Error: no se permiten numeros negativos" << endl;
+        return 1;
+    } else if (numero % 2 == 0) {
         cout << "El numero es PAR" << endl;
     } else {
         cout << "El numero es IMPAR" << endl;
